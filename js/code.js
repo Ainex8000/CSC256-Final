cab1415b14b9dc435a72da234b6f58d9e241f008
Xenia DeNoyer <90346767+Ainex8000@users.noreply.github.com>
2023-04-16 23:05:08 -0700

    Add files via upload

--- js/code.js
+++ js/code.js
@@ -6,18 +6,18 @@ window.onload = initLinks;
 
 // Array to hold photos of the dogs
 var thePic = 0;
-var arrMyPix = new Array("./img/oliver2.png","./img/alfred.png", "./img/winston.png")
+var arrMyPix = new Array("./img/oliver2.png", "./img/alfred.png", "./img/winston.png", "./img/xeniaimg4.jpg", "./img/sisters.jpg", "./img/family.jpg", "./img/gradphoto.jpg")
 
 // Function to initialize the links
-function initLinks(){
+function initLinks() {
     document.getElementById("prevLink").onclick = processPrevious;
     document.getElementById("nextLink").onclick = processNext;
 }
 
 // Function to process the previous link
-function processPrevious(){
-    if (thePic == 0){
-        thePic = arrMyPix.lngeth;
+function processPrevious() {
+    if (thePic == 0) {
+        thePic = arrMyPix.length;
     }
 
     thePic--;
@@ -26,37 +26,36 @@ function processPrevious(){
 }
 
 // Function to process the next link
-function processNext(){
+function processNext() {
     thePic++;
-    if (thePic == arrMyPix.length){
+    if (thePic == arrMyPix.length) {
         thePic = 0;
     }
 
     document.getElementById("puppy").src = arrMyPix[thePic];
 }
 
-
 // Display text functions for the portfolio.html page
-function displayObjOne(){
+function displayObjOne() {
     document.getElementById("myText").innerHTML = "Follow a software development process to analyze a problem and to design, build and test software solutions."
 }
 
-function displayObjTwo(){
+function displayObjTwo() {
     document.getElementById("myTextTwo").innerHTML = "Demonstrate software development skills using more than one programming language and development environment."
 }
 
-function displayObjThree(){
+function displayObjThree() {
     document.getElementById("myTextThree").innerHTML = "Implement data-driven solutions."
 }
 
-function displayObjFour(){
+function displayObjFour() {
     document.getElementById("myTextFour").innerHTML = "Design and implement software solutions for mobile devices, embedded systems and/or multiple platforms."
 }
 
-function displayObjFive(){
+function displayObjFive() {
     document.getElementById("myTextFive").innerHTML = "Document the planning and development of software applications using industry standard tools, techniques and processes."
 }
 
-function displayObjSix(){
+function displayObjSix() {
     document.getElementById("myTextSix").innerHTML = "Within software solutions describe, implement and analyze data structure techniques such as lists, trees, hash tables and graphs, along with sorting and searching algorithms."
 }
